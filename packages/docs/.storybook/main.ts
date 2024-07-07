e97eeffaf77c8a7fa9389d06103ce9052f2893c0
Gabriel Brum da Luz <biel.brumluz@gmail.com>
2024-07-07 13:57:13 -0300

    feat: add deploy-docs.yml

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,6 +25,13 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: true,
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = 'design-system-from-gabe-brum'
+    }
+
+    return config
   }
 };
 export default config;
